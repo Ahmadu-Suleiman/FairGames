2b6d8f1a140fb19a7b026f90e04a0ccaea817290
Ahmadu-Suleiman <ahmadumeta4.1@gmail.com>
2024-09-30 13:07:13 +0100

    add tic tac toe structure

---
+++ android/app/src/main/kotlin/com/example/fairgames/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fairgames
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:fairgames/tic_tac_toe.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Fair Games',
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)),
+        home: const TicTacToe());
+  }
+}
